00967089141079c0192a417134854494d5ed9501
theorivero <thrivero1@gmail.com>
2022-02-09 21:35:01 -0300

    Fineshed new login page

--- login-v2/src/App.jsx
+++ login-v2/src/App.jsx
@@ -1,23 +1,36 @@
-import jpImg from './assets/jp.svg'
+import { useState } from 'react';
+import pingaImg from './assets/ping-a.png'
 import './styles.css'
 
 function App() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
   return (
     <div className="container">
       <div className="container-login">
         <div className="wrap-login">
           <form className="login-form">
-              <span className="login-form-title">Bem Vindo!</span>
               <span className="login-form-title">
-                <img src={jpImg} alt="" />  
+                <img src={pingaImg} alt="" />  
               </span>
 
               <div className="wrap-input">
-                <input className='input' type="email" />
+                <input 
+                  className={email !== "" ? 'has-val input': 'input'} 
+                  type="email" 
+                  value={email}
+                  onChange={e =>{setEmail(e.target.value)}}
+                />
                 <span className="focus-input" data-placeholder='Email'></span>
               </div>
               <div className="wrap-input">
-                <input className='input' type="password" />
+                <input 
+                  className={password !== "" ? 'has-val input': 'input'} 
+                  type="password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
                 <span className="focus-input" data-placeholder='Password'></span>
               </div>
 
